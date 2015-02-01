4b769194332af177c6409ac74214eb3cf16be1e8
Chris Jennings <boz@cox.net>
2015-02-01 08:49:53 -0800

    Removes comments from original example sketches.

--- vibe-sensor.ino
+++ vibe-sensor.ino
@@ -160,30 +160,11 @@ void knock_setup() {
 
 void lcd_setup()
 {
-  // The LiquidCrystal library can be used with many different
-  // LCD sizes. We're using one that's 2 lines of 16 characters,
-  // so we'll inform the library of that:
-  
+  // setup display size
   lcd.begin(16, 2);
-
-  // Data sent to the display will stay there until it's
-  // overwritten or power is removed. This can be a problem
-  // when you upload a new sketch to the Arduino but old data
-  // remains on the display. Let's clear the LCD using the
-  // clear() command from the LiquidCrystal library:
 
   lcd.clear();
 
-  // Now we'll display a message on the LCD!
-  
-  // Just as with the Arduino IDE, there's a cursor that
-  // determines where the data you type will appear. By default,
-  // this cursor is invisible, though you can make it visible
-  // with other library commands if you wish.
-  
-  // When the display powers up, the invisible cursor starts 
-  // on the top row and first column.
-  
   lcd.print("Vibrations:");
 
   // Adjusting the contrast (IMPORTANT!)
@@ -243,63 +224,14 @@ void knock_loop() {
   }
 }
 
-void lcd_loop()
-{
-  // You can move the invisible cursor to any location on the
-  // LCD before sending data. Counting starts from 0, so the top
-  // line is line 0 and the bottom line is line 1. Columns range
-  // from 0 on the left side, to 15 on the right.
-
-  // In additon to the "hello, world!" printed above, let's
-  // display a running count of the seconds since the Arduino
-  // was last reset. Note that the data you send to the display
-  // will stay there unless you erase it by overwriting it or
-  // sending a lcd.clear() command.
-  
-  // Here we'll set the invisible cursor to the first column
-  // (column 0) of the second line (line 1):
-
+void lcd_loop() {
+  // display vibe count
   lcd.setCursor(12,0);
-
-  // Now we'll print the number of seconds (millis() / 1000)
-  // since the Arduino last reset:
-
   lcd.print(vibeCount);
 
-  // TIP: Since the numeric data we're sending is always growing
-  // in length, new values will always overwrite the previous ones.
-  // However, if you want to display varying or decreasing numbers
-  // like a countdown, you'll find that the display will leave
-  // "orphan" characters when the new value is shorter than the
-  // old one.
-  
-  // To prevent this, you'll need to erase the old number before
-  // writing the new one. You can do this by overwriting the
-  // last number with spaces. If you erase the old number and
-  // immediately write the new one, the momentary erase won't 
-  // be noticeable. Here's a typical sequence of code:
-  
-  // lcd.setCursor(0,1);   // Set the cursor to the position
-  // lcd.print("       "); // Erase the largest possible number
-  // lcd.setCursor(0,1);   // Reset the cursor to the original position
-  // lcd.print(millis()/1000); // Print our value
-  
-  // NEXT STEPS:
 
-  // Now you know the basics of hooking up an LCD to the Arduino,
-  // and sending text and numeric data to the display!
 
-  // The LCD library has many commands for turning the
-  // cursor on and off, scrolling the screen, etc. See:
-  // http://arduino.cc/en/Reference/LiquidCrystal
-  // for more information.
   
-  // Arduino also comes with a number of built-in examples
-  // showing off the features of the LiquidCrystal library.
-  // These are locted in the file/examples/LiquidCrystal menu.
-
-  // Have fun, and let us know what you create!
-  // Your friends at SparkFun.
 }
 
 void loop() {
